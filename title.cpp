fabfc9169dc60720daffe1727d25cdc2d793ec26
ColinAslett5 <280929@bsd48.org>
2017-02-24 21:07:58 -0800

    THIRD COMMIT

--- title.cpp
+++ title.cpp
@@ -1,56 +1,58 @@
-//Colin Aslett, Shunting Yard Algorithm, C++ Period 07
-//http://www.cprogramming.com/tutorial/lesson15.html
-//http://www.sanfoundry.com/cpp-program-implement-stack-linked-list/
+//Colin Aslett, C++ Period 07, Shunting Yard Algorithm
 #include <iostream>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 using namespace std;
 struct Node{
   char fir[1];
   Node* next;
-  int pre;//precedence
+  int pre;
 };
 Node* head = NULL;
-void pop(char* ch,int a, char* as);
-void push(char* ch, int a, char* as);
+void push(char* ch,int a,char* as);
+Node* peek();
+Node* pop();
 int main(){
+  cout << "HELLO!" << endl;
   char input[100];
-  cout << "Enter a expression" << endl;
-  cin >> input;
+  cin.getline(input,100);
+  //taking out spaces
   bool kg = false;
   int i = 0;
   size_t length = strlen(input);
-  cout << "length of input: " << length << endl;
+  //cout << "length of input: " << length << endl;
   char ans[length];
   ans[0] = '\0';
   while(kg == false){
     push(input,i,ans);
     i++;
     if(i == length){
-      if(i == strlen(input)){
-	Node* el = head;
-	while(head != NULL){
-	  size_t leng = strlen(input);//input queue
-	  size_t opp = strlen(ans);//output queue
-	  if(el->next != NULL){
-	    el = el->next;
-	    cout << "searching" << endl;
-	  }
-	  if(el->next == NULL){
-	    ans[opp] = el->fir[0];
-	    ans[opp+1] = '\0';
-	    cout << "final queue: " << ans << endl;
-	    //el = head;
-	  }
+      //end of queue
+      while(head != NULL){
+	Node* check = head;
+	Node* prev = head;
+	cout << "THE END THE END" << endl;
+	size_t len = strlen(ans);
+	ans[len] = check->fir[0];
+	ans[len+1] = '\0';
+	if(check->next != NULL){
+	  prev = check->next;
 	}
+	else{
+	  prev = NULL;
+	}
+	//check = NULL;
+	delete check;
+	head = prev;
+	cout << "FINAL ANSWER: " << ans << endl;
       }
       kg = true;
     }
   }
   return 0;
 }
-void push(char* ch, int a, char* as){
+void push(char* ch,int a,char* as){
   if(isdigit(ch[a])){
     cout << "digit" << endl;
     size_t len = strlen(as);
@@ -59,22 +61,6 @@ void push(char* ch, int a, char* as){
     cout << "output queue: " << as << endl;
   }
   if(ch[a] == '-' || ch[a] == '+' || ch[a] == '*' || ch[a] == '/' || ch[a] == '^'){
-    /*
-    Node* current = head;
-    if(current == NULL){
-      head = new Node;
-      head->fir[0] = ch[a];
-      cout << "operand" << endl;
-    }
-    else{
-      while(current->next != NULL){
-	current = current->next;
-      }
-      current->next = new Node;
-      current->next->fir[0] = ch[a];
-      cout << "second phase operator" << current->fir[0] << endl;
-    }
-    */
     int prec;//precedence
     if(ch[a] == '-'){
       prec = 2;
@@ -91,69 +77,96 @@ void push(char* ch, int a, char* as){
     if(ch[a] == '^'){
       prec = 4;
     }
-    //check for popping
-    bool con = true;
-    Node* check;
-    while(con == true){
-      check = head;
-      if(check != NULL){
-	while(check->next != NULL){
-	  check = check->next;
-	}
-	if(check->pre > prec || check->pre == prec){
-	  //con = true; stays true
-	  pop(ch,a,as);
-	  con = false;
-	  cout << "lower precedence" << endl;
-	}
-	else{
-	  con = false;
-	  cout << "higher precedence" << endl;
-	}
-      }
-      else{
-	con = false;
-	break;
-      }
-    }
-    //node setting
     Node* current = head;
     if(current == NULL){
       head = new Node;
       head->next = NULL;
       head->fir[0] = ch[a];
       head->pre = prec;
-      cout << "first operand!" << endl;
     }
     else{
-      while(current->next != NULL){
-	current = current->next;
+      while(peek()->pre > prec || peek()->pre == prec){
+	if(peek()->pre == 4 & prec == 4){
+	  break;
+	}
+	else{
+	  cout << "hello" << endl;
+	  Node* nn = pop();
+	  size_t leng = strlen(as);
+	  as[leng] = nn->fir[0];
+	  as[leng+1] = '\0';
+	  delete nn;
+	}
+        if(peek() == NULL){
+	  break;
+	}
       }
-      current->next = new Node;
-      current = current->next;
-      current->next = NULL;
-      current->fir[0] = ch[a];
-      current->pre = prec;
-      cout << "another operand" << endl;
+      Node* temp = new Node;
+      temp->next = head;
+      temp->fir[0] = ch[a];
+      temp->pre = prec;
+      head = temp;
+      cout << "NEW NODEs" << endl;
     }
-    //printing out the stack
     Node* prin = head;
     while(prin != NULL){
       cout << prin->fir[0] << " , " << prin->pre << endl;
       prin = prin->next;
-    }
-    //clearing out the stack
+    }    
+  }
+  if(ch[a] == ')' || ch[a] == '('){
+    
   }
 }
-void pop(char* ch, int a, char* as){
-  Node* current = head;
-  size_t len = strlen(as);
-  while(current->next != NULL){
-    current = current->next;
+Node* peek(){
+  return head;
+  /*
+  if(check->pre > prec || check->pre == prec){
+    pop(prec,ch,a,as);
   }
-  as[len] = current->fir[0];
+  else{
+    //continue on
+    head = new Node;
+    head->next = NULL;
+    head->fir[0] = ch[a];
+    head->pre = prec;
+  }
+  */
+}
+Node* pop(){
+  /*
+  Node* check = head;
+  Node* prev = head;
+  cout << "pop" << endl;
+  size_t len = strlen(as);
+  as[len] = check->fir[0];
   as[len+1] = '\0';
-  current = NULL;
-  delete current;
-  cout << "UPDATED queue: " << as << endl;
+  if(check->next != NULL){
+    prev = check->next;
+  }
+  else{
+    prev = NULL;
+  }
+  check = NULL;
+  delete check;
+  head = prev;
+  cout << "ans:" << as;
+  if(head == NULL){
+    //continue on
+    //Node* current = head;
+    head = new Node;
+    head->next = NULL;
+    head->fir[0] = ch[a];
+    head->pre = prec;
+  }
+  else{
+    peek(prec,ch,a,as);
+  }
+  */
+  if(head == NULL){
+    return NULL;
+  }
+  Node* temp = head;
+  head = head->next;
+  return temp;
 }
